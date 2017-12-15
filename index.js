0fe15df1a0f6d5bbcc75cecba04ae036450f3463
Alan Verbner <alverbner@gmail.com>
2017-12-15 16:40:58 -0300

    Prettier

--- index.js
+++ index.js
@@ -3,28 +3,32 @@
 const repl = require("repl");
 const Web3 = require("web3");
 const utils = require("ethereumjs-util");
-const program = require('commander');
+const program = require("commander");
 const chalk = require("chalk");
 const web3Admin = require("./web3Admin");
-const os = require('os');
+const os = require("os");
 
-const path = require('path');
+const path = require("path");
 
 const package = require("./package.json");
 
-const defaultProvider = "http://localhost:8545"
+const defaultProvider = "http://localhost:8545";
 
-const isETCFork = function (web3) {
+const isETCFork = function(web3) {
   const forkBlock = web3.eth.getBlock(1920000);
   if (forkBlock) {
-    if (forkBlock.hash === '0x94365e3a8c0b35089c1d1195081fe7489b528a84b22199c916180db8b28ade7f') return "true"
+    if (
+      forkBlock.hash ===
+      "0x94365e3a8c0b35089c1d1195081fe7489b528a84b22199c916180db8b28ade7f"
+    )
+      return "true";
     else return "false";
   } else {
     return "?";
   }
 };
 
-const printHeader = function () {
+const printHeader = function() {
   console.log(`
 ██╗    ██╗███████╗██████╗ ██████╗         ██╗███████╗    ██████╗ ███████╗██████╗ ██╗     
 ██║    ██║██╔════╝██╔══██╗╚════██╗        ██║██╔════╝    ██╔══██╗██╔════╝██╔══██╗██║     
@@ -32,17 +36,17 @@ const printHeader = function () {
 ██║███╗██║██╔══╝  ██╔══██╗ ╚═══██╗   ██   ██║╚════██║    ██╔══██╗██╔══╝  ██╔═══╝ ██║     
 ╚███╔███╔╝███████╗██████╔╝██████╔╝██╗╚█████╔╝███████║    ██║  ██║███████╗██║     ███████╗
  ╚══╝╚══╝ ╚══════╝╚═════╝ ╚═════╝ ╚═╝ ╚════╝ ╚══════╝    ╚═╝  ╚═╝╚══════╝╚═╝     ╚══════╝
-`)
-}
+`);
+};
 
-const printStatus = (web3) => () => {
+const printStatus = web3 => () => {
   try {
     const lastBlockNumber = web3.eth.blockNumber;
     const lastBlock = web3.eth.getBlock(lastBlockNumber);
     console.log(`
   ${chalk.green("Provider         : " + web3.currentProvider.host)}
   ${chalk.green("Last Block #     : " + lastBlockNumber)}
-  ${chalk.green("Last Block       : " + (!lastBlock? 'N/A' : lastBlock.hash))}
+  ${chalk.green("Last Block       : " + (!lastBlock ? "N/A" : lastBlock.hash))}
   ${chalk.green("Mining?          : " + web3.eth.mining)}
   ${chalk.green("Peer Count       : " + web3.net.peerCount)}
   ${chalk.green("Web3 api version : " + web3.version.api)}
@@ -52,18 +56,23 @@ const printStatus = (web3) => () => {
   } catch (err) {
     console.error(chalk.red(err));
   }
-}
+};
 
 program
   .version(package.version)
-  .option('-p, --provider [url]', 'Web3JS RPC provider')
-  .option('-s, --skip-status', 'Does not show status after bootstrap')
-  .option('-h, --history-file [path]', 'File path of commands history file (defaults to $HOME/.web3_repl_history)')
+  .option("-p, --provider [url]", "Web3JS RPC provider")
+  .option("-s, --skip-status", "Does not show status after bootstrap")
+  .option(
+    "-h, --history-file [path]",
+    "File path of commands history file (defaults to $HOME/.web3_repl_history)"
+  )
   .parse(process.argv);
 
 printHeader();
 
-const web3 = new Web3(new Web3.providers.HttpProvider(program.provider || defaultProvider));
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(program.provider || defaultProvider)
+);
 if (!program.skipStatus) printStatus(web3)();
 
 var replServer = repl.start({
@@ -71,11 +80,11 @@ var replServer = repl.start({
   ignoreUndefined: true
 });
 
-const historyFile = program.historyFile ? 
-program.historyFile : 
-path.join(os.homedir(), '.web3_repl_history');
+const historyFile = program.historyFile
+  ? program.historyFile
+  : path.join(os.homedir(), ".web3_repl_history");
 
-require('repl.history')(replServer, historyFile);
+require("repl.history")(replServer, historyFile);
 
 web3Admin.extend(web3);
 
